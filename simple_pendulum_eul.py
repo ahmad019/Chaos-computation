4119b4f311c7886803c74a527af97603f978b1f3
ahmad019 <ahmdbilx@gmail.com>
2018-07-18 00:10:23 +0500

    modified

--- simple_pendulum_eul.py
+++ simple_pendulum_eul.py
@@ -11,7 +11,7 @@ equation: 0''=(-g/l)*sin0
 """
 import matplotlib.pyplot as plt
 import numpy as np
-from math import sin 
+
 
 SAMPLES=50000
 
@@ -19,41 +19,46 @@ SAMPLES=50000
 
 time = np.ndarray(SAMPLES, float) #array for time steps
 theti = np.ndarray(SAMPLES, float)   #for 0 values
-pi = np.ndarray(SAMPLES, float)     #for 0' (derivative)
+di = np.ndarray(SAMPLES, float)     #for 0' (derivative)
 
 
 
-p = 0;
-h = 0.001;
-theta= 10;
+d = 0;
+h = 0.01;
+theta= 2.96;
 t = 0;
-w = 1;
+l=0.5;
+w = 9.81/l;
+
 
 i = 0;
 
 while i<=SAMPLES - 1:
-
-    U = -w*sin(theta)
-    #U = -w*theta
-    eul = p + h*U
-    eul2 = theta + h*p
     
-    p = eul;
+    w = 9.81/l;
+
+    U = -(w**2)*np.sin(theta)
+   # U = -(w**2)*theta
+    eul = d + h*U
+    eul2 = theta + h*d
+    
+    d = eul;
     t = t + h;
     theta = eul2;
     
     time[i]=t;
     theti[i]=eul2;
-    pi[i]=eul;
+    di[i]=eul;
     
     i+=1
     
-#plt.title("displacement vs time")
-#plt.plot(time,theti, 'r')
-#plt.show()
-#plt.title("derivative vs time")
-#plt.plot(time,pi, 'b')
+plt.title("displacement vs time")
+plt.plot(time,theti, 'r')
+plt.show()
+plt.title("derivative vs time")
+plt.plot(time,di, 'b')
+plt.show()
 plt.title("phase plot")
-plt.plot(theti,pi, 'orange')
+plt.plot(theti,di, 'orange')
 plt.show()
 
--- simplependulum_phase.py
+++ simplependulum_phase.py
@@ -7,7 +7,11 @@ Created on Thu Jul 12 15:09:12 2018
 import numpy as np
 import matplotlib.pyplot as plt
 
-SAMPLES=50000
+steps=8
+density=1000
+
+SAMPLES=density*steps #we need 10000 samples persecond so 
+    
 
 time = np.ndarray(SAMPLES, float) #array for time steps
 theti = np.ndarray(SAMPLES, float)   #for 0 values
@@ -16,18 +20,18 @@ pi = np.ndarray(SAMPLES, float)     #for 0' (derivative)
 
 
 p0 = 0;
-theta0 = 10;
+theta0 = np.pi / 4;
 h0 = 0.001;
-t0 = 0;
-w = 1;
+t0 = 0
+w = 1
 
 U = -w*np.sin(theta0)
 
 def U(theta):
-    return -w * theta
+    return -w**2 * theta
 
 def Usin(theta):
-    return -w * np.sin(theta)
+    return -w**2 * np.sin(theta)
 
 
 def euler(p,theta,h,t,c,u):
@@ -48,9 +52,13 @@ def euler(p,theta,h,t,c,u):
         
         i+=1
         
-    plt.plot(theti,pi,color=c)
+    plt.plot(theti,pi,color=c) #phase plot
+    
+    #plt.plot(time,theti, color=c)
+   # plt.show()
+    #plt.plot(time,pi, color=c)
     
 
-euler(p0,theta0,h0,t0,'b', Usin)
-euler(p0,theta0,h0,t0,'y', U)
+#euler(p0,theta0,h0,t0,'b', Usin)
+euler(p0,theta0,h0,t0,'m', U)
 
